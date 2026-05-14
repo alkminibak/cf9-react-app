374700bc872151b763b85e11b13baf391c994bed
alkminibak <alkmini.bak@gmail.com>
2026-05-14 20:55:18 +0300

    feat: add CounterAdvanced component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 import Layout from "./components/Layout.tsx";
-import Counter from "./components/LessonFour/Counter.tsx";
-import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
+import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
+// import Counter from "./components/LessonFour/Counter.tsx";
+// import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 
 // type PropsA = {
 //     title: string,
@@ -74,8 +75,9 @@ function App() {
 
        {/*LESSON FOUR */}
        <Layout addClasses="bg-gray-50">
-          <Counter/>
-          <ClassCounter/>
+          {/*<Counter/>*/}
+          {/*<ClassCounter/>*/}
+          <CounterAdvanced/>
        </Layout>
     </>
   )
