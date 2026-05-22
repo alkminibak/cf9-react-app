139b42cd72fa2961b2e0830606e2e87b38667db5
alkminibak <alkmini.bak@gmail.com>
2026-05-22 21:05:06 +0300

    chore: use Link instead a tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
+import {Link} from "react-router";
 
 const Header = () => {
    return (
       <>
          <header className="bg-cf-dark-red fixed w-full">
             <div className="container mx-auto px-4 flex items-center justify-between">
-               <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
-                    alt="CF Logo" className="my-4 h-16"/>
+               <Link to="/">
+                  <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
+                       alt="CF Logo" className="my-4 h-16"/>
+               </Link>
 
                <nav className="flex gap-4 text-white font-medium">
-                  <a href="/">Home</a>
+                  <Link to="/">Home</Link>
                </nav>
             </div>
          </header>
