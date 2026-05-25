77fd03a55fa2648a0613c9fbcfead79a917fd389
alkminibak <alkmini.bak@gmail.com>
2026-05-25 21:01:37 +0300

    feat: add MultiFieldFormWithValidation component

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import RouterLayout from "./components/RouterLayout.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
 import MultiFieldForm from "./components/LessonNine/MultiFieldForm.tsx";
+import MultiFieldFormWithValidation from "./components/LessonNine/MultiFieldFormWithValidation.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 
@@ -164,6 +165,7 @@ function App() {
              {/*  search?query=react&page=3   Query Params  */}
 
              <Route path="multifield-form" element={<MultiFieldForm />} />
+             <Route path="multifield-form-validation" element={<MultiFieldFormWithValidation />} />
 
           </Route>
           {/* /files/* */}
